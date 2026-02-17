de6613ef9b018784ced71d3ac39760e2010a0e45
Synn <synn@Synns-MacBook-Pro.local>
2026-02-17 21:14:01 +0000

    Generated unity inputsystem, added gameinput script

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GameInput : MonoBehaviour
+{
+    public static GameInput instance;
+    public event EventHandler OnJump;
+
+    private InputSystem_Actions inputActions;
+
+    private void Awake()
+    {
+        SingletonPattern();
+        inputActions = new InputSystem_Actions();
+        inputActions.Enable();
+
+        inputActions.Player.Jump.performed += Jump_Performed;
+    }
+
+    private void SingletonPattern()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Jump_Performed(InputAction.CallbackContext context)
+    {
+        OnJump.Invoke(this, EventArgs.Empty);
+    }
+
+    public int Move()
+    {
+        // implement move
+        return 0;
+    }
+}
