0cf495a20070ac46724bfe1ab09b9d0ab6cd7422
Synn <synn@Synns-MacBook-Pro.local>
2026-02-18 01:12:06 +0000

    started jump animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,10 +13,13 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Animator playerAnimator;
 
     private Rigidbody2D playerRB;
+    private BoxCollider2D playerFeetCollider; // yummy
 
     private void Start()
     {
         playerRB = gameObject.GetComponent<Rigidbody2D>();
+        playerFeetCollider = gameObject.GetComponent<BoxCollider2D>();
+        GameInput.instance.OnJump += Jump;
     }
 
     private void Update()
@@ -25,9 +28,14 @@ public class PlayerController : MonoBehaviour
         FlipSprite();
     }
 
-    private void Jump()
+    private void Jump(object sender, EventArgs e)
     {
-        
+        int groundLayer = LayerMask.GetMask("Ground");
+
+        if (playerFeetCollider.IsTouchingLayers(groundLayer))
+        {
+            playerRB.linearVelocity += new Vector2 (0f, jumpSpeed);
+        }
     }
 
     private void Move()
