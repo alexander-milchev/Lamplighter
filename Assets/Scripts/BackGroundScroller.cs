539824118b77d895787484ac0d85033c886de550
Synn <synn@Synns-MacBook-Pro.local>
2026-02-18 19:28:20 +0000

    Created sorting layers and parallax script, recut leaf assets

---
+++ Assets/Scripts/BackGroundScroller.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class BackGroundScroller : MonoBehaviour
+{
+    private Vector2 startPos;
+    [SerializeField] private GameObject cam;
+    [SerializeField] private float parallaxEffect;
+
+    private void Start()
+    {
+        startPos = transform.position; 
+    }
+
+    private void Update()
+    {
+        Vector2 distance = cam.transform.position * parallaxEffect;
+
+        transform.position = new Vector3(startPos.x + distance.x, startPos.y + distance.y, transform.position.z);
+    }
+}
