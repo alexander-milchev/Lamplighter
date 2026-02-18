5f3476c1ea177ef7f32cafdfcdb3dcd2a1323568
Synn <synn@Synns-MacBook-Pro.local>
2026-02-18 15:34:38 +0000

    Created double jump, imported cinemachine

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,10 +16,17 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D playerRB;
     private BoxCollider2D playerFeetCollider; // yummy
 
+    private int maxJumps = 2;
+    private int jumpCount;
+    private int groundLayer;
+
+
     private void Start()
     {
         playerRB = gameObject.GetComponent<Rigidbody2D>();
         playerFeetCollider = gameObject.GetComponent<BoxCollider2D>();
+        groundLayer = LayerMask.GetMask("Ground");
+
         GameInput.instance.OnJump += Jump;
     }
 
@@ -31,13 +38,19 @@ public class PlayerController : MonoBehaviour
 
     private void Jump(object sender, EventArgs e)
     {
-        int groundLayer = LayerMask.GetMask("Ground");
-
+        // Reset jump count
         if (playerFeetCollider.IsTouchingLayers(groundLayer))
         {
-            playerRB.linearVelocity += new Vector2 (0f, jumpSpeed);
+            jumpCount = 0;
+        }
+
+        if (jumpCount < maxJumps)
+        {
+            playerRB.linearVelocity = new Vector2 (0f, jumpSpeed);
+            jumpCount++;
             playerAnimator.SetBool("isJumping", true);
-            StartCoroutine(WaitToLand());
+
+            if (jumpCount == 1){StartCoroutine(WaitToLand());}
         }
     }
 
@@ -64,10 +77,8 @@ public class PlayerController : MonoBehaviour
 
     private IEnumerator WaitToLand()
     {
-        int groundlayer = LayerMask.GetMask("Ground");
-
-        yield return new WaitUntil(() => !playerFeetCollider.IsTouchingLayers(groundlayer));
-        yield return new WaitUntil(() => playerFeetCollider.IsTouchingLayers(groundlayer));
+        yield return new WaitUntil(() => !playerFeetCollider.IsTouchingLayers(groundLayer));
+        yield return new WaitUntil(() => playerFeetCollider.IsTouchingLayers(groundLayer));
 
         playerAnimator.SetBool("isJumping", false);
     }
